808abea250ea8408cca4cb00ddeac91cc06e8fff
J C <yarr0@asgard.local>
2023-12-25 22:50:59 +0100

    init scripts

--- init-pwnbox.sh
+++ init-pwnbox.sh
@@ -25,19 +25,8 @@ if [ ! -d $WDIR/mate-terminal ]; then
   mkdir -p mate-terminal
 fi
 
-# init user environment
-
-cp -af $HOME/my_data/home/. $HOME
-fontDir="$HOME/.local/share/fonts"
-mkdir -p $fontDir
-cp "$HOME/my_data/fonts/*.ttf" $fontDir
-
-if which fc-cache >/dev/null 2>&1 ; then
-  fc-cache -f "$fontDir"
-fi
-
-dconf load /org/mate/terminal/profiles/profile0/ < $WDIR/mate-terminal/profile0.mtconf
-dconf load /org/mate/terminal/global/ < $WDIR/mate-terminal/global.mtconf
+wget https://raw.githubusercontent.com/y4rr0/configs/main/user_init.sh  --backups=1
+chmod +x user_init
 
 # return to the previous working directory
 cd $PREV_PWD
